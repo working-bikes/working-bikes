451cee7f89670497ed96372faddc64d9f82a44c1
Dane Hillard <dane@danehillard.com>
2015-02-25 23:32:21 -0500

    Add points awards for volunteers

---
+++ volunteer/migrations/0013_auto__add_pointsaward.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'PointsAward'
+        db.create_table(u'volunteer_pointsaward', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('volunteer', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['volunteer.Volunteer'])),
+            ('points', self.gf('django.db.models.fields.IntegerField')()),
+            ('reason', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'volunteer', ['PointsAward'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'PointsAward'
+        db.delete_table(u'volunteer_pointsaward')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'volunteer.pointsaward': {
+            'Meta': {'ordering': "['volunteer__user__first_name']", 'object_name': 'PointsAward'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'points': ('django.db.models.fields.IntegerField', [], {}),
+            'reason': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'volunteer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['volunteer.Volunteer']"})
+        },
+        u'volunteer.purchase': {
+            'Meta': {'ordering': "['volunteer__user__first_name']", 'object_name': 'Purchase'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'points': ('django.db.models.fields.IntegerField', [], {}),
+            'volunteer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['volunteer.Volunteer']"})
+        },
+        u'volunteer.purchaseapproval': {
+            'Meta': {'object_name': 'PurchaseApproval'},
+            'approved_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'purchase': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['volunteer.Purchase']", 'unique': 'True'})
+        },
+        u'volunteer.task': {
+            'Meta': {'object_name': 'Task'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'members_only': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'volunteer.timesheet': {
+            'Meta': {'object_name': 'Timesheet'},
+            'day': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
+            'from_event': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hours': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['volunteer.Task']", 'null': 'True', 'blank': 'True'}),
+            'volunteer': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['volunteer.Volunteer']"})
+        },
+        u'volunteer.timesheetapproval': {
+            'Meta': {'object_name': 'TimesheetApproval'},
+            'approved_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'timesheet': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['volunteer.Timesheet']", 'unique': 'True'})
+        },
+        u'volunteer.volunteer': {
+            'Meta': {'ordering': "['user__first_name']", 'object_name': 'Volunteer'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'country': ('django.db.models.fields.CharField', [], {'default': "'United States'", 'max_length': '50'}),
+            'emergency_contact': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'emergency_contact_phone': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'preferred_tasks': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['volunteer.Task']", 'null': 'True', 'blank': 'True'}),
+            'skills': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'street_address': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'Volunteer'", 'max_length': '50'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
+            'zip_code': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        }
+    }
+
+    complete_apps = ['volunteer']
