68321d68e816e57e1c0f22b5f9b17f466eae64ff
Dane Hillard <github@danehillard.com>
2015-03-02 16:09:56 -0500

    Add gunicorn to requirements.txt. Get settings from environment variables.

--- working_bikes/settings.py
+++ working_bikes/settings.py
@@ -1,30 +1,25 @@
-"""
-Django settings for working_bikes project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import socket
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+PROJECT_NAME = 'WORKING_BIKES'
+PROJECT_VARIABLE_PATTERN = '_'.join((PROJECT_NAME, '{}'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '17=$bz2%+)a1jj!7bsdo=#$@c!o5@!(pfx659h(4!^w-0dpxmu'
+SECRET_KEY = os.getenv(PROJECT_VARIABLE_PATTERN.format('SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv(PROJECT_VARIABLE_PATTERN.format('DEBUG'), False) == 'TRUE'
 TEMPLATE_DEBUG = DEBUG
 
+ADMINS = (
+    ('Dane Hillard', 'github@danehillard.com'),
+)
+
+MANAGERS = ADMINS
+
+ADMIN_URL = os.getenv(PROJECT_VARIABLE_PATTERN.format('ADMIN_URL'), 'r^admin/')
+
 TEMPLATE_DIRS = (
-	BASE_DIR + '/templates',
+	os.path.join(BASE_DIR, 'templates'),
 )
 
 LOGIN_REDIRECT_URL = '/volunteer/profile/'
@@ -64,41 +59,21 @@ WSGI_APPLICATION = 'working_bikes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'working_bikes',
-		'USER': 'working_bikes',
-		'PASSWORD': 'w0rk1ngb1k35',
-		'HOST': '127.0.0.1',
-		'PORT': '',
+        'NAME': os.getenv(PROJECT_VARIABLE_PATTERN.format('DATABASE_NAME')),
+		'USER': os.getenv(PROJECT_VARIABLE_PATTERN.format('DATABASE_USER')),
+		'PASSWORD': os.getenv(PROJECT_VARIABLE_PATTERN.format('DATABASE_PASSWORD')),
+		'HOST': os.getenv(PROJECT_VARIABLE_PATTERN.format('DATABASE_HOST'), 'localhost'),
+		'PORT': os.getenv(PROJECT_VARIABLE_PATTERN.format('DATABASE_PORT'), 3306),
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + '/static'
-
-if socket.gethostname() == 'danehillard':
-	DEBUG = False
-	ALLOWED_HOSTS = ['workingbikes.danehillard.com',]
-	DATABASES['default']['HOST'] = 'production.csnsdgwkxnzo.us-east-1.rds.amazonaws.com'
-else:
-	import mimetypes
-	ALLOWED_HOSTS = ['localhost',]
-
-	mimetypes.add_type("image/svg+xml", ".svg", True)
-	mimetypes.add_type("image/svg+xml", ".svgz", True)
+STATIC_URL = os.getenv(PROJECT_VARIABLE_PATTERN.format('STATIC_URL'), '/static/')
+STATIC_ROOT = os.getenv(PROJECT_VARIABLE_PATTERN.format('STATIC_ROOT'), 'static')
+MEDIA_URL = os.getenv(PROJECT_VARIABLE_PATTERN.format('MEDIA_URL'), '/media/')
+MEDIA_ROOT = os.getenv(PROJECT_VARIABLE_PATTERN.format('MEDIA_ROOT'), 'media')
