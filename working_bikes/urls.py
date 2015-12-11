e5894e21c6f2b6b8857ff6966ee8a5a6628b2fb5
Dane Hillard <dane@danehillard.com>
2015-12-10 21:55:38 -0500

    Upgrade to Django 1.9

--- working_bikes/urls.py
+++ working_bikes/urls.py
@@ -7,12 +7,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(settings.ADMIN_URL, include(admin.site.urls)),
     url(r'^volunteer/', include('volunteer.urls', namespace='volunteer')),
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('volunteer:index'))),
     url(r'^explorer/', include('explorer.urls')),
-)
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('volunteer:index'))),
+]
 
 urlpatterns += staticfiles_urlpatterns()
