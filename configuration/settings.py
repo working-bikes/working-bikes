ccf5a3d44660c71625619ec63c61b5036c33b2d6
Dane Hillard <github@danehillard.com>
2017-05-09 16:59:21 -0400

    Add rollbar

--- configuration/settings.py
+++ configuration/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 )
 
 ROLLBAR = {
