ff2a39e551b1191f819b234414b54cc54772596b
Dane Hillard <github@danehillard.com>
2017-12-23 23:03:25 -0500

    Avoid Django 2.0 for now

--- configuration/urls.py
+++ configuration/urls.py
@@ -3,12 +3,12 @@ from django.conf import settings
 from django.views.generic import RedirectView
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import reverse_lazy, path
+from django.urls import reverse_lazy
 
 admin.autodiscover()
 
 urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
+    url(settings.ADMIN_URL, admin.site.urls),
     url(r'^volunteer/', include('volunteer.urls')),
     url(r'^explorer/', include('explorer.urls')),
     url(r'^$', RedirectView.as_view(url=reverse_lazy('volunteer:index'))),
